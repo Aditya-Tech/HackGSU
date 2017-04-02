dd02de4eed808c6a74863b3f772e152e814c4687
Aditya Paruchuri <ap376@duke.edu>
2017-04-01 21:46:45 -0400

    this is kicking my ass, but getting closer

---
+++ js/app.js
@@ -0,0 +1,44 @@
+(function() {
+    var SpeechRecognition = SpeechRecognition || webkitSpeechRecognition || mozSpeechRecognition || msSpeechRecognition;
+
+    // End if SpeechRecognition api not available in Browser
+    if(!SpeechRecognition){
+        alert('Your Browser dosen\'t support');
+        return;
+    }
+
+    var speech = new SpeechRecognition();
+    speech.lang = 'en-US';
+
+    // if any error occour
+    speech.onerror = function(event) {
+        if(event.error == 'not-allowed') {
+            alert('Please allow microphone.');
+        } else {
+            alert('There is an error. Please see your console');
+            console.log(event);
+        }
+    };
+
+    // on result event;
+    speech.onresult = function(event) {
+        document.querySelector('#editer').value = event.results[0][0].transcript;
+        toggle();
+    };
+
+    //on speak button click
+    document.querySelector('#speek').addEventListener('click', function(e){
+        e.preventDefault();
+        toggle();
+        speech.start();
+    });
+
+
+    var nowflg = true;
+    function toggle() {
+        document.querySelector('#speek-now').style.visibility = nowflg? 'hidden': 'visible';
+        nowflg = !nowflg;
+    }
+    toggle();
+
+}());
